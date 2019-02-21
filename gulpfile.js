829b6b25c20b9926a7e54e5e553424c8bbd4a013
zhy <1711668523@qq.com>
2019-02-21 08:53:46 +0800

    提交

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp=require('gulp');
+var server=require('gulp-webserver');
+
+gulp.task('server',function(){
+    return gulp.src('./src/')
+    .pipe(server({
+        port:8888,
+        open:true,
+        livereload:true
+    }))
+})
